4096f69456033201e941e6ae5424a3321a538872
maib07001 <maib07001@E-PW05C37V>
2024-09-12 13:49:43 +0200

    Add project files.

---
+++ test1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace Uppgift_1._3
+{
+    class program
+  
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("\"Hej\"Hörde jag någon skrika ut på stan.");
+            Console.WriteLine("Efter en stund svarade någon annan också med ett \"Hej!\".");
+
+        }
+
+
+
+
+    }
+        
+
+
+}
